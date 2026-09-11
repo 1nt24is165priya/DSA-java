8f86d4f80e7ee7ae437dee3c675bfa15197afaf3
Priya R C <1nt24is165.priya@nmit.ac.in>
2026-09-12 00:06:18 +0530

    Factorial of n using Recursion

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Factorial{
+
+    public static int factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+         return n*factorial(n-1); 
+        }
+    }
+
+    public static void main(String args[]){
+        System.out.println("Enter value of n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("factorial of " + n + " is: " + factorial(n));
+        
+
+    }
+}
